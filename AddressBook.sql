7c271c9266d8c9310a199bbc7a6a441dd9b72668
pranav21999 <pranavdani99@gmail.com>
2021-07-24 02:26:13 +0530

    Retrieved:person using state or city

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,7 +1,34 @@
-
-#uc5
-#Ability to delete person using FirstName
-#Query:-
-DELETE FROM `address_book` 
+#UC6
+#Ability to Retrieve Person belonging to a City or State from the Address Book
+#query:-
+#1)Retrive contact using city 
+SELECT 
+    *
+FROM
+    address_book
 WHERE
-    `firstName` = 'aaa';
+    city = 'pune';
+    #2)Retrive contact using city
+SELECT 
+    *
+FROM
+    address_book
+WHERE
+    city = 'Anagar';
+    
+	#3)Retrive contact using city
+SELECT 
+    *
+FROM
+    address_book
+WHERE
+    city = 'amravati';
+
+#4)Retrive contact using state
+SELECT 
+    *
+FROM
+    address_book
+WHERE
+    state = 'maharastra';
+
