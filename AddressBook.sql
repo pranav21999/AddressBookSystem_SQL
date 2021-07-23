ce3118503afde5eee7b2a70273c2201371d71684
pranav21999 <pranavdani99@gmail.com>
2021-07-24 02:49:17 +0530

    Sorted:address_book Alphabetically By their firstName

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,34 +1,8 @@
-#UC6
-#Ability to Retrieve Person belonging to a City or State from the Address Book
-#query:-
-#1)Retrive contact using city 
+#uc8
+#Ability to retrieve entries sorted alphabetically by Person’s name for a given city
+#Query-:
 SELECT 
     *
 FROM
     address_book
-WHERE
-    city = 'pune';
-    #2)Retrive contact using city
-SELECT 
-    *
-FROM
-    address_book
-WHERE
-    city = 'Anagar';
-    
-	#3)Retrive contact using city
-SELECT 
-    *
-FROM
-    address_book
-WHERE
-    city = 'amravati';
-
-#4)Retrive contact using state
-SELECT 
-    *
-FROM
-    address_book
-WHERE
-    state = 'maharastra';
-
+ORDER BY firstName;
