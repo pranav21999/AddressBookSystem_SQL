fd26449bd199e81e9890e32d1965f6c2ae23a68f
pranav21999 <pranavdani99@gmail.com>
2021-07-24 01:36:11 +0530

    created:Table address_book with Attributes(firstName,lastName,city,state,address,zip,phoneNo,email)

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,14 +1,11 @@
-#uc2
-#Ability to create a Address Book Table with first andlast names, address, city,state, zip, phone numberand email as its attributes.
-#Query:-
-create table address_book(
-	firstName varchar(30)  not null ,
-    lastName varchar(30)  not null ,
-    address varchar(50)  not null,
-    city varchar(20)  not null,
-	state varchar(20)  not null,
-	zip  varchar(7) not null,
-	phoneNo varchar(7) not null,
-    email varchar(7) not null,
-	primary key(firstName)
-);
+#uc3
+#Ability to insert new Contacts to Address Book
+#Query-:
+insert  into address_book(firstName,lastname,address,city,state,zip,phoneNo,email) values
+ ('pranav','dani','Tilak Road','pune','Maharastra','411030','93929393','aaa@gmail.com'),
+ ('sanket','abc','Gandhi Road','Anagar','Maharastra','414039','343929393','abc@gmail.com'),
+ ('aachal','makode','sb road','amravati','Maharastra','311030','783929393','xyz@gmail.com'),
+ ('aaa','bbb',' road','mumbai','Maharastra','321030','723929393','xyzw@gmail.com');
+ #display Contacts in address_book table
+#Query-: 
+select * from address_book;
