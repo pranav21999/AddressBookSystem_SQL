670de4a3a459e60b93d5a143e26386b7a3950b51
pranav21999 <pranavdani99@gmail.com>
2021-07-24 03:15:10 +0530

    Added:get no of contact by Type

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,6 +1,4 @@
-#uc9
-#Ability to identify each Address Book with name andType.
-alter table address_book add type varchar(30) after lastName;
-update  address_book set type='family' where firstName ='pranav';
-update  address_book set type='friend' where firstName ='aachal';
-update  address_book set type='profession' where firstName ='sanket';
+#UC10
+# COUNT(city) AS NumberOfaddress_book FROM address_book;
+#query 
+select count(type) AS NumberOfaddress_book FROM address_book;
